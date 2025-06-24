c955a1b79983f370508d946211b6874d4e5426a7
manavguu44 <133528017+manavguu44@users.noreply.github.com>
2025-06-25 01:56:07 +0530

    app.py

--- app.py
+++ app.py
@@ -55,3 +55,10 @@ if uploaded_file:
     
     # Download link for metadata
     st.download_button("Download JSON metadata", json.dumps(metadata, indent=2), file_name="metadata.json", mime="application/json")
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
